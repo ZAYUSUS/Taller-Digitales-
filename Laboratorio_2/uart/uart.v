0c83366e0fcc629892d149ed5acbd7a0c3888b68
ZAYUSUS <esquivelbryan3@gmail.com>
2024-09-13 17:25:31 -0600

    Se modificó el código de la Uart, se debe realizar las pruebas con la Fpga en físico

--- Laboratorio_2/uart/uart.v
+++ Laboratorio_2/uart/uart.v
@@ -2,25 +2,25 @@
 
 module uart
 #(
-    parameter DELAY_FRAMES = 234 // 27,000,000 (27Mhz) / 115200 Baud rate
+    parameter DELAY_FRAMES = 2812 // 27,000,000 (27Mhz) / 9600 Baud rate
 )
 (
     input clk,
     input uart_rx,
     output uart_tx,
-    output reg [5:0] led,
-    input btn1
+    output reg [5:0] led
 );
 
 localparam HALF_DELAY_WAIT = (DELAY_FRAMES / 2);
 
-reg [3:0] rxState = 0;
-reg [12:0] rxCounter = 0;
-reg [7:0] dataIn = 0;
-reg [2:0] rxBitNumber = 0;
-reg byteReady = 0;
+reg [3:0] rxState = 0; // estado actual
+reg [12:0] rxCounter = 0; // pulsos de reloj
+reg [7:0] dataIn = 0;// bit recivido
+reg [2:0] rxBitNumber = 0; // bits leidos
+reg byteReady = 0; // indica cuando se leyo un byte (8 bits)
 
-localparam RX_STATE_IDLE = 0;
+// maquina de estados
+localparam RX_STATE_IDLE = 0; 
 localparam RX_STATE_START_BIT = 1;
 localparam RX_STATE_READ_WAIT = 2;
 localparam RX_STATE_READ = 3;
@@ -28,7 +28,7 @@ localparam RX_STATE_STOP_BIT = 5;
 
 always @(posedge clk) begin
     case (rxState)
-        RX_STATE_IDLE: begin
+        RX_STATE_IDLE: begin // estado inactivo
             if (uart_rx == 0) begin
                 rxState <= RX_STATE_START_BIT;
                 rxCounter <= 1;
@@ -68,53 +68,44 @@ always @(posedge clk) begin
         end
     endcase
 end
+// fin recepción de datos-----------------------------------------
 
 always @(posedge clk) begin
     if (byteReady) begin
         led <= ~dataIn[5:0];
     end
 end
-
-reg [3:0] txState = 0;
-reg [24:0] txCounter = 0;
-reg [7:0] dataOut = 0;
-reg txPinRegister = 1;
-reg [2:0] txBitNumber = 0;
-reg [3:0] txByteCounter = 0;
+// comienzo transmision de datos --------------------------------------------
+reg [3:0] txState = 0;// estado actual
+reg [24:0] txCounter = 0;// ciclos de reloj
+reg [7:0] dataOut = 0;// byte que se envía
+reg txPinRegister = 1;// valor a adjuntar en uart_tx
+reg [2:0] txBitNumber = 0;// bit enviando
 
 assign uart_tx = txPinRegister;
 
-localparam MEMORY_LENGTH = 12;
-reg [7:0] testMemory [MEMORY_LENGTH-1:0];
+// parte a cambiar
+reg  [1:0] fila = 0;
+reg [1:0] columna = 0;
+reg select = 0;
+reg [7:0] teclado[0:3][0:3] =  {{'1','2','3','A'},
+                                {'4','5','6','B'},
+                                {'7','8','9','C'},
+                                {'*','0','#','D'}}
 
-initial begin
-    testMemory[0] = "L";
-    testMemory[1] = "u";
-    testMemory[2] = "s";
-    testMemory[3] = "h";
-    testMemory[4] = "a";
-    testMemory[5] = "y";
-    testMemory[6] = " ";
-    testMemory[7] = "L";
-    testMemory[8] = "a";
-    testMemory[9] = "b";
-    testMemory[10] = "s";
-    testMemory[11] = " ";
-end
+//localparam MEMORY_LENGTH = 2;
 
 localparam TX_STATE_IDLE = 0;
 localparam TX_STATE_START_BIT = 1;
 localparam TX_STATE_WRITE = 2;
 localparam TX_STATE_STOP_BIT = 3;
 localparam TX_STATE_DEBOUNCE = 4;
-
-always @(posedge clk) begin
+always @(posedge clk)begin
     case (txState)
         TX_STATE_IDLE: begin
-            if (btn1 == 0) begin
+            if (select == 0) begin //cuando se presiona una tecla
                 txState <= TX_STATE_START_BIT;
                 txCounter <= 0;
-                txByteCounter <= 0;
             end
             else begin
                 txPinRegister <= 1;
@@ -124,7 +115,7 @@ always @(posedge clk) begin
             txPinRegister <= 0;
             if ((txCounter + 1) == DELAY_FRAMES) begin
                 txState <= TX_STATE_WRITE;
-                dataOut <= testMemory[txByteCounter];
+                dataOut <= teclado[fila][columna];// guardo el caracter
                 txBitNumber <= 0;
                 txCounter <= 0;
             end else 
@@ -143,26 +134,22 @@ always @(posedge clk) begin
             end else 
                 txCounter <= txCounter + 1;
         end
-        TX_STATE_STOP_BIT: begin
+            TX_STATE_STOP_BIT: begin
             txPinRegister <= 1;
             if ((txCounter + 1) == DELAY_FRAMES) begin
-                if (txByteCounter == MEMORY_LENGTH - 1) begin
-                    txState <= TX_STATE_DEBOUNCE;
-                end else begin
-                    txByteCounter <= txByteCounter + 1;
-                    txState <= TX_STATE_START_BIT;
-                end
+                txState <= TX_STATE_DEBOUNCE;
                 txCounter <= 0;
             end else 
                 txCounter <= txCounter + 1;
         end
-        TX_STATE_DEBOUNCE: begin
+            TX_STATE_DEBOUNCE: begin
             if (txCounter == 23'b111111111111111111) begin
-                if (btn1 == 1) 
+                if (select == 1) 
                     txState <= TX_STATE_IDLE;
             end else
                 txCounter <= txCounter + 1;
         end
-    endcase      
+    endcase
 end
+
 endmodule
