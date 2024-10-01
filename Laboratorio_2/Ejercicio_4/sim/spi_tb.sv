c637e674d8658260195eec12064a82994056149e
ZAYUSUS <esquivelbryan3@gmail.com>
2024-09-30 21:14:16 -0600

    Se modificó spi para mostrar un patrón de columnas, falta la implementación con la UART

--- Laboratorio_2/Ejercicio_4/sim/spi_tb.sv
+++ Laboratorio_2/Ejercicio_4/sim/spi_tb.sv
@@ -1,4 +1,38 @@
 module spi_tb (
 );
-    
+reg clk;
+reg resetn;
+
+initial
+begin
+	clk = 0;
+	forever #1 clk = ~clk;
+end
+
+initial begin
+	resetn = 1;
+	#10
+	resetn = 0;
+	#10
+	resetn = 1;
+	#2000
+	resetn = 0;
+	#10
+	resetn = 1;
+	#2000 $stop;
+end
+
+Spi s(
+	.clk(clk),
+	.resetn(resetn),
+
+	.ser_tx(),
+	.ser_rx(),
+
+	.lcd_resetn(),
+	.lcd_clk(),
+	.lcd_cs(),
+	.lcd_rs(),
+	.lcd_data()
+);
 endmodule
